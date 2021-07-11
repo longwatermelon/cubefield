400d909a3fd643ec176456bb8475c85955c8ef5d
longwatermelon <longwatermelon1@gmail.com>
2021-07-11 14:16:44 -0700

    game over sequence

--- src/cubefield.c
+++ src/cubefield.c
@@ -5,6 +5,7 @@
 struct Game* game_init()
 {
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
     struct Game* game = malloc(sizeof(struct Game));
     game->window = SDL_CreateWindow("Cubefield", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
@@ -13,11 +14,15 @@ struct Game* game_init()
     SDL_RenderClear(game->rend);
     SDL_RenderPresent(game->rend);
 
+    game->font = TTF_OpenFont("res/font.ttf", 16);
+
     game->cube_list = 0;
     game->cubes_num = 0;
 
     game->speed = 0.02f;
     game->x_velocity = 0.f;
+
+    game->alive = 1;
 
     srand(time(0));
 
@@ -37,7 +42,11 @@ void game_render(struct Game* game)
         struct Cube* cube = &game->cube_list[i];
 
         if (cube->points[0].z > 0.f && cube->points[0].z - game->speed <= 0.f && cube->points[0].x <= 0.f && cube->points[1].x >= 0.f)
-            printf("game over\n"); 
+        {
+            game->alive = 0;
+            game->speed = 0.02f;
+            game->x_velocity = 0.f;
+        }
 
         cube_render(game->rend, cube);
         cube_move(cube, 0.f, 0.f, -game->speed);
@@ -67,7 +76,6 @@ void game_render(struct Game* game)
                 );
 
                 reused_cube = 1;
-
                 break;
             }
         }
@@ -76,7 +84,31 @@ void game_render(struct Game* game)
             append_cube(game, (float)randint(-1000, 1000) / 100.f, 0.4f, 10.f);
     }
 
-    game->speed += 0.00004f;
+    if (game->alive)
+        game->speed += 0.00004f;
+
+    if (!game->alive)
+    {
+        SDL_SetRenderDrawBlendMode(game->rend, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderDrawColor(game->rend, 0, 0, 0, 150);
+
+        SDL_Rect rect = { 0, 0, 800, 800 };
+        SDL_RenderFillRect(game->rend, &rect);
+
+        SDL_SetRenderDrawBlendMode(game->rend, SDL_BLENDMODE_NONE);
+
+        SDL_Texture* tex = render_text(game->rend, game->font, "Game over", (SDL_Color) { 255, 255, 255 });
+        SDL_Rect tmp = { 350, 300 };
+        TTF_SizeText(game->font, "Game over", &tmp.w, &tmp.h);
+        SDL_RenderCopy(game->rend, tex, 0, &tmp);
+        SDL_DestroyTexture(tex);
+
+        tex = render_text(game->rend, game->font, "Press space to try again", (SDL_Color) { 255, 255, 255 });
+        tmp = (SDL_Rect){ 300, 400 };
+        TTF_SizeText(game->font, "Press space to try again", &tmp.w, &tmp.h);
+        SDL_RenderCopy(game->rend, tex, 0, &tmp);
+        SDL_DestroyTexture(tex);
+    }
 
     SDL_SetRenderDrawColor(game->rend, 0, 0, 0, 255);
     SDL_RenderPresent(game->rend);
@@ -98,10 +130,21 @@ int game_handle_events(struct Game* game)
             switch (evt.key.keysym.sym)
             {
             case SDLK_RIGHT:
-                game->x_velocity = 0.03f;
+                if (game->alive)
+                    game->x_velocity = 0.03f;
                 break;
             case SDLK_LEFT:
-                game->x_velocity = -0.03f;
+                if (game->alive)
+                    game->x_velocity = -0.03f;
+                break;
+            case SDLK_SPACE:
+                if (!game->alive)
+                {
+                    game->alive = 1;
+                    free(game->cube_list);
+                    game->cube_list = 0;
+                    game->cubes_num = 0;
+                }
                 break;
             }
         }
@@ -130,11 +173,13 @@ int game_handle_events(struct Game* game)
 void game_quit(struct Game* game)
 {
     free(game->cube_list);
+    TTF_CloseFont(game->font);
 
     SDL_DestroyRenderer(game->rend);
     SDL_DestroyWindow(game->window);
 
     SDL_Quit();
+    TTF_Quit();
 }
 
 
@@ -167,6 +212,19 @@ static struct Cube create_cube(float x, float y, float z)
             { x - size, y + size, z }
         }
     };
+}
+
+
+static SDL_Texture* render_text(SDL_Renderer* rend, TTF_Font* font, const char* text, SDL_Color color)
+{
+    if (!text[0])
+        return 0;
+
+    SDL_Surface* surf = TTF_RenderText_Blended(font, text, color);
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surf);
+    SDL_FreeSurface(surf);
+
+    return tex;
 }
 
 
--- src/cubefield.h
+++ src/cubefield.h
@@ -3,6 +3,7 @@
 
 #include "cube.h"
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 
 struct Game
@@ -10,12 +11,15 @@ struct Game
     SDL_Window* window;
     SDL_Renderer* rend;
 
+    TTF_Font* font;
+
     struct Cube* cube_list;
     size_t cubes_num;
 
     float speed;
-
     float x_velocity;
+
+    int alive;
 };
 
 struct Game* game_init();
@@ -26,6 +30,9 @@ int game_handle_events(struct Game* game);
 
 static void append_cube(struct Game* game, float x, float y, float z);
 static struct Cube create_cube(float x, float y, float z);
+
+static SDL_Texture* render_text(SDL_Renderer* rend, TTF_Font* font, const char* text, SDL_Color color);
+
 inline static int randint(int min, int max);
 
 #endif
